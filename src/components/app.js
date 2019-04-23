91ded17c0881fb8dda539ecc38ab897e07920ebe
Tom <tcantinelli@gmail.com>
2019-04-24 01:27:21 +0200

    Start squelette Modal

--- src/components/app.js
+++ src/components/app.js
@@ -3,28 +3,63 @@ import M from "materialize-css";
 //Components
 import Header from "../components/header";
 import BigPart from "../components/bigPart";
-//Containers
-import Signin from "../containers/signin";
 
 require("../style.css");
 
 class App extends Component {
-	    componentWillMount () { 
-	    	document.addEventListener("DOMContentLoaded", function () {
-	    		var elems = document.querySelectorAll(".modal");
-	    		M.Modal.init(elems, {});
-	    	});
-	    }
+	componentWillMount () {
+		document.addEventListener("DOMContentLoaded", function () {
+			var elems = document.querySelectorAll(".modal");
+			M.Modal.init(elems, {});
+		});
+	}
 
-	  render () {
-	  	return (
-	  		<div className="container-fluid">
-	  			<Header />
-	  			<BigPart />
-	  			<Signin toto={"ruuu"} />			
-	  		</div>
-	  	);
-	  }
+	render () {
+		return (
+			<div className="container-fluid">
+				<Header />
+				<BigPart />
+				<div id="modal1" class="modal loginModal">
+					<div class="modal-content">
+						<form className="col s8">
+							<div className="row rowModal">
+								<div className="input-field col s12">
+									<i className="material-icons prefix">account_circle</i>
+									<input id="email" type="email" className="validate" />
+									<label for="email">Email</label>
+									<span class="helper-text" data-error="Email incorrect" data-success="Email validé">Validation</span>
+								</div>
+							</div>
+							<div className="row rowModal">
+								<div className="input-field col s12">
+									<i className="material-icons prefix">https</i>
+									<input id="password" type="password" className="validate" />
+									<label for="password">Mot de passe</label>
+								</div>
+							</div>
+							<div className="row rowModal">
+								<div className="input-field col s6 offset-s3">
+									<p className="center-align">
+										<label>
+											<input type="checkbox" />
+											<span>Se souvenir</span>
+										</label>
+									</p>
+								</div>
+							</div>
+							<div className="row rowModal">
+								<div className="input-field col s6 offset-s3 center-align">
+									<button class="btn waves-effect" type="submit" name="action">Connexion
+										<i class="material-icons right">send</i>
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,17 +4,19 @@ class Header extends Component {
 	render () {
 		return (
 			<div className="row valign-wrapper">
-				<div className="col s11">
+				<div className="col s7 m9 xl10">
 					<img
 						className="responsive-img headerImage"
-						src={"../img/Titre.png"}
+						src={
+							"../img/Titre.png"
+						}
 						alt={
 							"tool.title"
 						}
 					/>
 				</div>
-				<div className="col s1 buttonCol">
-					<button data-target="loginModal" className="btn modal-trigger">Connexion</button>
+				<div className="col s5 m3 xl2">
+					<button data-target="modal1" className="btn modal-trigger buttonCol">Se connecter</button>
 				</div>
 			</div>
 		);
